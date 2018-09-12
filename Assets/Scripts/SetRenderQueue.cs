5aca9c2a7f579c770e48b6e9f443bbcc41ccf5e1
Mystogan98 <shangheilis@hotmail.fr>
2018-09-12 16:50:41 +0200

    Fix scale + autre

---
+++ Assets/Scripts/SetRenderQueue.cs
@@ -0,0 +1,24 @@
+﻿/*
+	SetRenderQueue.cs
+ 
+	Sets the RenderQueue of an object's materials on Awake. This will instance
+	the materials, so the script won't interfere with other renderers that
+	reference the same materials.
+*/
+
+using Unity;
+using UnityEngine;
+ 
+[AddComponentMenu("Rendering/SetRenderQueue")]
+public class SetRenderQueue : MonoBehaviour {
+ 
+	[SerializeField]
+	protected int m_queues = 3000;
+
+	protected void Awake() {
+		Material[] materials = GetComponent<Renderer>().materials;
+		for (int i = 0; i < materials.Length; ++i) {
+			materials[i].renderQueue = m_queues;
+		}
+	}
+}
